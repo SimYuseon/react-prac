e74aa42bd546d39ca7073226b0092f43012a2ce8
Yuseon Sim <yseon0508@naver.com>
2022-08-25 13:40:00 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import DayList from "./components/DayList";
+import Header from "./components/Header";
+import Day from "./components/Day";
+import EmptyPage from "./components/EmptyPage";
+import CreateWord from "./components/CreateWord";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import CreateDay from "./components/CreateDay";
+// import Word from "./component/Word";
+// import Words from ""
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<DayList />} />
+          <Route path="/day/:day" element={<Day />} />
+          <Route path="/create_word" element={<CreateWord />} />
+          <Route path="/create_day" element={<CreateDay />} />
+          <Route path="*" element={<EmptyPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
